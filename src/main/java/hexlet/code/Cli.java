161b8bcb9d4e19c9866593f62a1e158d10e648dd
Dmitri Chuprin <wassermanum@gmail.com>
2022-05-30 17:04:26 +0600

    feat: created Makefile and added greetings

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetings() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = in.nextLine();
+        System.out.printf("Hello, %s \n", name);
+    }
+}
