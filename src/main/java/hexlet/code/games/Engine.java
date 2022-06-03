79c3e97b3ff7fdce72426080bd9427d6d41d7361
Dmitri Chuprin <wassermanum@gmail.com>
2022-06-04 04:27:01 +0600

    fix: move the rest logic from App to Engine

--- src/main/java/hexlet/code/games/Engine.java
+++ src/main/java/hexlet/code/games/Engine.java
@@ -1,9 +1,11 @@
 package hexlet.code.games;
 
+import hexlet.code.Game;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Supplier;
 
-public class Engine {
+public final class Engine {
     public static void runGame(List<String> questions, List<String> answers, String name) {
         Scanner in = new Scanner(System.in);
         for (int i = 0; i < questions.size(); i++) {
@@ -21,5 +23,32 @@ public class Engine {
                 System.out.printf("Congratulations, %s!\n", name);
             }
         }
+    }
+    public static void start() {
+        System.out.println("Welcome to the Brain Games!");
+        List<Supplier<? extends Game>> list = List.of(
+                Exit::new,
+                Greeting::new,
+                Even::new,
+                Calculator::new,
+                GreatestCommonDivider::new,
+                Progression::new
+        );
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1. Greeting.");
+        System.out.println("2. Even.");
+        System.out.println("3. Calculator.");
+        System.out.println("4. GCD.");
+        System.out.println("5. Progression.");
+        System.out.println("0. Exit.");
+        System.out.print("Your choose: ");
+        Scanner in = new Scanner(System.in);
+        int gameChoice = in.nextInt();
+        if (gameChoice < 0 || gameChoice >= list.size()) {
+            System.out.println("Wrong choice!");
+            System.exit(0);
+        }
+        Supplier<? extends Game> ctor = list.get(gameChoice);
+        ctor.get().play();
     }
 }
